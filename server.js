d1ba7f6c122ca9d127ca6b71cdfe1e9e672dcd1e
honaz140 <honaz140@gmail.com>
2023-04-09 12:03:36 +0700

    'a'

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Horray! Your Express server is up and running.')
 })
 
-app.listen(port, () => {
+app.listen(port , () => {
   console.log(`Express app listening on port ${port}`)
 })
